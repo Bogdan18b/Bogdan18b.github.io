7de3e811c8aab6438d515d1b097f800744bdb024
bogdan <bogdanbobletec@gmail.com>
2019-09-14 08:03:19 -0400

    Updates

---
+++ precache-manifest.384c5aa6f94de60fb64f65cad4970151.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d93e834345b3333831b163aba7ea7fd",
+    "url": "/static/media/ike.9d93e834.png"
+  },
+  {
+    "revision": "7124ab8cc8a137f56181",
+    "url": "/static/css/main.e290b6d6.chunk.css"
+  },
+  {
+    "revision": "63c070f66ed43272e5cf",
+    "url": "/static/js/1.63c070f6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c9877ee3fb16ad25c2e24950e257bfb0",
+    "url": "/static/media/fam.c9877ee3.jpg"
+  },
+  {
+    "revision": "7124ab8cc8a137f56181",
+    "url": "/static/js/main.7124ab8c.chunk.js"
+  },
+  {
+    "revision": "e144f5553fc893af96843ed90f61273f",
+    "url": "/static/media/button.e144f555.png"
+  },
+  {
+    "revision": "897436d87caefa20adf9c57ef3f75bd2",
+    "url": "/static/media/weather.897436d8.png"
+  },
+  {
+    "revision": "95d4e7e61f12b967a3f7c505afdab8f5",
+    "url": "/static/media/piata.95d4e7e6.png"
+  },
+  {
+    "revision": "67b3c256745f49a9a78e2cf9fad97475",
+    "url": "/static/media/trianglify2.67b3c256.svg"
+  },
+  {
+    "revision": "e138f9ed3ab986f53a2d645dd68efc56",
+    "url": "/index.html"
+  }
+];
