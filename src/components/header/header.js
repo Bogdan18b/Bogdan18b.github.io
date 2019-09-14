65e9da153705a12b3ab5d0f60846ce5ee7ad179d
bogdan <bogdanbobletec@gmail.com>
2019-09-14 08:01:50 -0400

    add WIP message

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import HeaderList from './headerStyle';
+import React from "react";
+import HeaderList from "./headerStyle";
 
 const Header = () => (
   <HeaderList>
     <li>
-      <a href="mailto:bogdanbobletec@gmail.com"><i className="fa fa-envelope"></i></a>
+      <a href="mailto:bogdanbobletec@gmail.com">
+        <i className="fa fa-envelope"></i>
+      </a>
     </li>
     <li>
-      <a href="https://www.linkedin.com/in/bogdanbobletec/"><i className="fab fa-linkedin"></i></a>
+      <a href="https://www.linkedin.com/in/bogdanbobletec/">
+        <i className="fab fa-linkedin"></i>
+      </a>
     </li>
     <li>
-      <a href="https://github.com/Bogdan18b"><i className="fab fa-github"></i></a>
-    </li>
-    <li>
-      <a href="https://angel.co/bogdan-bobletec"><i className="fab fa-angellist"></i></a>
+      <a href="https://github.com/Bogdan18b">
+        <i className="fab fa-github"></i>
+      </a>
     </li>
   </HeaderList>
 );
--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import MainDiv from './mainStyle';
-import Pdf from '../../resume.pdf';
-import Typist from 'react-typist';
+import React, { Component } from "react";
+import MainDiv from "./mainStyle";
+import Typist from "react-typist";
 
 class Title extends Component {
   state = {
@@ -10,42 +9,29 @@ class Title extends Component {
     showProjects: false
   };
   updateStyle = () => {
-    this.titleRef.current.style.gridColumn = '1/3';
-    this.titleRef.current.style.gridRow = '1/2';
-    this.titleRef.current.children[0].style.flexDirection = 'row';
-    this.titleRef.current.children[0].style.flex = '1';
-    this.titleRef.current.querySelector('h2').textContent = '';
+    this.titleRef.current.style.gridColumn = "1/3";
+    this.titleRef.current.style.gridRow = "1/2";
+    this.titleRef.current.children[0].style.flexDirection = "row";
+    this.titleRef.current.children[0].style.flex = "1";
+    this.titleRef.current.querySelector("h2").textContent = "";
   };
   titleRef = React.createRef(null);
   handleShow = () => {
     this.updateStyle();
     this.props.showAbout();
-  }
+  };
   handleProjects = () => {
     this.updateStyle();
     this.props.showProjects();
-  }
+  };
   render() {
     return (
       <MainDiv ref={this.titleRef}>
-        <Typist className="middle"
-        >
+        <Typist className="middle">
           <h1 onClick={() => document.location.reload()}>BOGDAN BOBLETEC</h1>
           <br />
-          <h2>software developer from New York City</h2>
-          <div>
-            <button
-              onClick={this.handleShow}
-            >About&nbsp;&nbsp;-&nbsp;&nbsp;
-              </button>
-            <button
-              onClick={this.handleProjects}
-            >Projects&nbsp;&nbsp;-&nbsp;&nbsp;
-              </button>
-            <a href={Pdf} rel="noopener noreferrer"
-              target="_blank">Resume
-              </a>
-          </div>
+          <h2>software developer</h2>
+          <p>New and improved Gatsby version coming soon...</p>
         </Typist>
       </MainDiv>
     );
